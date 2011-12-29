45cd29a4666320a559a734feca2e85d90fe8142b
Felipe De Bene <fdebene@Felipe-De-Benes-MacBook.local>
2011-12-29 13:50:42 -0200

    changed charset

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module DepotB
     config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "iso-8859-1"
+    config.encoding = "UTF-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
