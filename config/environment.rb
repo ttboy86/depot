bcdc2df90e229ff96155821f9213125fbc2c606c
Felipe Bene <ttboy86@gmail.com>
2012-01-15 12:18:38 -0200

    "finished another chapter"

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DepotB::Application.initialize!
+
+DepotB::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.stmp_settings = {
+  
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "pragsex.com",
+  authentication: "plain",
+  user_name: "ttboy86",
+  password: "Xoxota!86",
+  enable_starttls_auto: true
+  }
+end
